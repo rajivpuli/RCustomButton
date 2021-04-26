29e9fe69034225e70ed45da9ddac574d6d661619
RaJiV <rajiv.p@photon.in>
2021-04-26 18:03:55 +0530

    Added test cases

---
+++ RCustomButton/RCustomButton.swift
@@ -0,0 +1,22 @@
+//
+//  RCustomButton.swift
+//  RCustomButton
+//
+//  Created by Rajiv Puli on 26/04/21.
+//
+
+import Foundation
+
+public final class RCustomButton {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- RCustomButtonTests/RCustomButtonTests.swift
+++ RCustomButtonTests/RCustomButtonTests.swift
@@ -10,6 +10,12 @@ import XCTest
 
 class RCustomButtonTests: XCTestCase {
 
+    var rCustomButton: RCustomButton!
+    
+    override func setUp() {
+        rCustomButton = RCustomButton()
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,6 +34,10 @@ class RCustomButtonTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testAdd(){
+        XCTAssertEqual(rCustomButton.add(a: 1, b: 1), 2)
     }
 
 }

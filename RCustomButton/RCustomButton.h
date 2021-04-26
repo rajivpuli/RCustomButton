dbca4a7a58823c9c8fab548588a3f82912c14db2
RaJiV <rajiv.p@photon.in>
2021-04-26 17:55:46 +0530

    Initial project setup

---
+++ RCustomButton/RCustomButton.h
@@ -0,0 +1,18 @@
+//
+//  RCustomButton.h
+//  RCustomButton
+//
+//  Created by Rajiv Puli on 26/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RCustomButton.
+FOUNDATION_EXPORT double RCustomButtonVersionNumber;
+
+//! Project version string for RCustomButton.
+FOUNDATION_EXPORT const unsigned char RCustomButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCustomButton/PublicHeader.h>
+
+

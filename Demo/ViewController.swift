f9844c0bb1cd843d8580e04c44d9a4ddea820c5e
RaJiV <rajiv.p@photon.in>
2021-04-26 20:11:08 +0530

    All attributes changed to public

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        let button = RButton()
+        button.shapeOfButton = .cirlce
     }
 
 
